6e24830f9aea851aa383dcbc6c588c4053eee1d5
tks18 <tksudharshan@gmail.com>
2025-10-26 23:04:23 +0530

    fix(app): remove unwanted import which caused problems

--- src/xl_pq_handler/__init__.py
+++ src/xl_pq_handler/__init__.py
@@ -1,9 +1,7 @@
-from .handler import XLPowerQueryHandler
 from .classes import PQManager, ExcelQueryService, PQFileStore, DependencyResolver, PowerQueryMetadata, PowerQueryScript
 from .ui import PQManagerUI
 
 __all__ = [
-    "XLPowerQueryHandler",
     "ExcelQueryService",
     "PQFileStore",
     "DependencyResolver",
